6659e0952b83831154c78fed3ab348e58e952f06
Unconscious <reverendo_siegfried@live.com.mx>
2020-09-18 14:34:10 -0500

    Initial commit

---
+++ controllers/TasksController.js
@@ -0,0 +1,27 @@
+const Task = require('../models/Task');
+
+exports.store = (req, res) => {
+  let task = {};
+  task.description = req.body.description;
+  Task.create(task).then((id) => {
+    console.log('Task created with id: ', id);
+    if (req.xhr || req.headers.accept.indexOf('json') > -1) {
+      Task.find(id).then((task) => res.json(task));
+    } else {
+      res.redirect('/');
+    }
+  });
+}
+
+exports.store = (req, res) => {
+  let task = {};
+  task.description = req.body.description;
+  Task.create(task).then((id) => {
+    console.log('Task created with id: ', id);
+    if (req.xhr || req.headers.accept.indexOf('json') > -1) {
+      Task.find(id).then((task) => res.json(task));
+    } else {
+      res.redirect('/');
+    }
+  });
+}
---
+++ models/Task.js
@@ -0,0 +1,22 @@
+const knex = require('../database/connection');
+
+exports.PENDING = 'pending';
+
+exports.all = () => {
+  return knex
+    .select('*')
+    .from('tasks');
+}
+
+exports.create = (task) => {
+  return knex('tasks')
+    .insert({ description: task.description });
+}
+
+exports.find = (id) => {
+  return knex
+    .select('*')
+    .from('tasks')
+    .where('id', id)
+    .first();
+}
